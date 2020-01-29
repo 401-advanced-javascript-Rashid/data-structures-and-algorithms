85d7eb2984edbe9a2d8b0b53f25280d0f7d666cb
Rashid-Alsawalqa <rashid.dmcr@gmail.com>
2020-01-29 22:14:27 +0200

    All Done

--- challenges/__tests__/array-shift.test.js
+++ challenges/__tests__/array-shift.test.js
@@ -4,13 +4,13 @@ const insertShiftArray = require('../arrayShift/array-shift.js');
 
 describe('Output', () => {
     
-    it('The number is pushed', () => {
-        let arr = [2,4,6,8];
-        expect(insertShiftArray(arr, 5).length).toBe(5);
-    });
+  it('The number is pushed', () => {
+    let arr = [2,4,6,8];
+    expect(insertShiftArray(arr, 5).length).toBe(5);
+  });
     
-    it('The number is pushed', () => {
-        let arr = [4,8,15,23,42];
-        expect(insertShiftArray(arr, 16).length).toBe(6);
-    });
-})
+  it('The number is pushed', () => {
+    let arr = [4,8,15,23,42];
+    expect(insertShiftArray(arr, 16).length).toBe(6);
+  });
+});
--- challenges/arrayShift/array-shift.js
+++ challenges/arrayShift/array-shift.js
@@ -2,20 +2,18 @@
 
 
 function insertShiftArray(arr, number) {
-    const mid = arr.length / 2;
-    const newArr = [];
-    let index = 0;
+  const mid = arr.length / 2;
+  const newArr = [];
+  let index = 0;
 
-    for (let i = 0; i < mid; i++) { newArr [ index ] = arr[i];
-        index += 1;
-    }
+  for (let i = 0; i < mid; i++) { newArr [ index ] = arr[i];
+    index += 1;
+  }
     
-    newArr[mid] = number;
-    for (let i = mid; i < arr.length; i++) { newArr [ newArr.length ] = arr[i];
-    }
-    return newArr;
+  newArr[mid] = number;
+  for (let i = mid; i < arr.length; i++) { newArr [ newArr.length ] = arr[i];
+  }
+  return newArr;
 }
-
-//   console.log(insertShiftArray(arr, number));
 
 module.exports = insertShiftArray;
